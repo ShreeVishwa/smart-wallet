adf62f82af6afe1abd2ded520536169de6e609aa
ShreeVishwa <reddyvishwatej176@gmail.com>
2020-08-12 20:44:52 -0400

    Divided the home screen into two parts. Closes #6

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -3,17 +3,45 @@ import {View, Text, StyleSheet} from 'react-native'
 
 const HomeScreen = () => {
     return (
-        <View style={styles.textStyle} >
-            <Text>Home Screen!</Text>
+        <View style={styles.containerStyle}>
+            <View style={styles.upperHalf}>
+
+            </View>
+            <View style={styles.lowerHalf}>
+                <View style={{flexDirection: 'row', flex: 1}}>
+                    <Text style={styles.leftText}>Recent Transactions</Text>
+                    <Text style={styles.rightText}>View all</Text>
+                </View>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    textStyle: {
-        flex:1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    upperHalf: {
+        flex: 0.4,
+        backgroundColor: 'white',
+    },
+    lowerHalf: {
+        flex: 0.6
+    },
+    containerStyle: {
+        flex: 1
+    },
+    leftText: {
+        left: 15,
+        marginTop: 20,
+        color: 'black',
+        position: 'absolute',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    rightText: {
+        right: 15, 
+        position: 'absolute',
+        color: 'blue',
+        marginTop: 20,
+        fontSize: 18
     }
 })
 
